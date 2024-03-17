bde460bd6ace859146207d2579ce694462b97e42
MathewsTanui <tanuimathews02@gmail.com>
2024-03-17 12:59:43 +0300

    complete index.html, script.js and style.css

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+document.addEventListener('DOMContentLoaded', function() {
+    function logAltText(event) {
+        console.log(event.target.alt);
+    }
+    const galleryImages = document.querySelectorAll('#gallery img');
+    galleryImages.forEach(function(image) {
+        image.addEventListener('click', logAltText);
+    });
+    const hamburger = document.querySelector('.hamburger');
+    const navLinks = document.querySelector('nav ul');
+    function toggleNav() {
+        navLinks.classList.toggle('show');
+    }
+    hamburger.addEventListener('click', toggleNav);
+});
